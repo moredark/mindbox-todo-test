858f233c4d077156ed237caf1cfac6573e66a799
da_sakmarkin <da_sakmarkin01@betboom.org>
2025-03-25 23:06:07 +0300

    style: Fix base path for icon link in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/mindbox-todo-test/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: './index.html',
-      },
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]',
         chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash][extname]'
+        entryFileNames: 'assets/[name].[hash].js'
       }
     }
   }
