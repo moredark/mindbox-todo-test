f45fa0975e471eb9e4b7617e6013596dd4d41dd0
da_sakmarkin <da_sakmarkin01@betboom.org>
2025-03-25 23:29:39 +0300

    fix: fix paths in index file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mindbox-todo-test/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
